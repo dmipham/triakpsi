d72e882feeacf0efdf10f434ffb7ff680958fe06
Paul Hiatt <hiattp@gmail.com>
2013-03-29 12:53:05 -0400

    added precompile settings for heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Demo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Heroku precompile assets setting
+    config.assets.initialize_on_precompile = false
+    
   end
 end
